3e72b8ba3552ba388dfae5568844ee390b4dfbb5
VisCram <126577181+VisCram@users.noreply.github.com>
2023-09-21 14:01:36 +0500

    add CarService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,21 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarService;
+import web.service.CarServiceImp;
 
 import java.util.List;
 
 @Controller
 public class CarController {
-    private final CarService carService;
+    private final CarServiceImp carServiceImp;
 
-    public CarController(CarService carService) {
-        this.carService = carService;
+    public CarController(CarServiceImp carServiceImp) {
+        this.carServiceImp = carServiceImp;
     }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(required = false, defaultValue = "5") int count, Model model) {
-        List<Car> cars = carService.getCars(count);
+        List<Car> cars = carServiceImp.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
