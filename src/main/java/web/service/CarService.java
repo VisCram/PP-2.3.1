f6433e91fca34019368fe168daf0ae8e8f6e7e9e
VisCram <126577181+VisCram@users.noreply.github.com>
2023-09-21 12:55:49 +0500

    add CarService

---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,30 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    private List<Car> cars;
+
+    public CarService() {
+        cars = new ArrayList<>();
+        cars.add(new Car("KIA", "RIO", 2020));
+        cars.add(new Car("Ford", "Mustang", 2021));
+        cars.add(new Car("Toyota", "Camry", 2020));
+        cars.add(new Car("Chevrolet", "Cruze", 2019));
+        cars.add(new Car("BMW", "X5", 2021));
+    }
+
+    public List<Car> getCars(int count) {
+        if (count >= 5) {
+            return cars;
+        } else {
+            return cars.subList(0, count);
+        }
+    }
+}
+
